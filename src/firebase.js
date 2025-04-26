3a2210f35e554226f067afcc699d7469c8bad2c1
Mia Jarrett <miajarrett333@gmail.com>
2025-04-25 18:22:10 -0700

    Fixed fatal error1

--- src/firebase.js
+++ src/firebase.js
@@ -1,13 +1,15 @@
-// Import the functions you need from the SDKs you need
+// src/firebase.js
+
 import { initializeApp } from "firebase/app";
-import { getStorage } from "firebase/storage"; // we'll use this to upload images
+import { getStorage } from "firebase/storage";
+import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyAAxSi0qzySAIN4luAfQteI29ghD7W1lho",
   authDomain: "plant-journal-app.firebaseapp.com",
   projectId: "plant-journal-app",
-  storageBucket: "plant-journal-app.appspot.com", // <-- corrected here
+  storageBucket: "plant-journal-app.appspot.com",
   messagingSenderId: "882193669237",
   appId: "1:882193669237:web:0998ed302d9141e46fac3c"
 };
@@ -15,5 +17,6 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
-// Export Storage so you can upload photos later
+// Export services
 export const storage = getStorage(app);
+export const db = getFirestore(app);
